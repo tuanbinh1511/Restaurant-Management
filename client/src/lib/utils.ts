49b4d6cba261d1758c4b02228e6aca7bcd1e9301
Dang Tuan Binh <tuanbinh1511@MacBook-Pro-cua-Dang.local>
2024-09-09 11:16:01 +0700

    build config , http

---
+++ client/src/lib/utils.ts
@@ -0,0 +1,12 @@
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+/**
+ * Xóa đi ký tự `/` đầu tiên của path
+ */
+export const normalizePath = (path: string) => {
+  return path.startsWith("/") ? path.slice(1) : path;
+};
